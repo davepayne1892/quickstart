cab15fc644d3fd0d0012b823ccccb4ad5bbfcf0c
davepayne1892 <47106774+davepayne1892@users.noreply.github.com>
2023-04-02 07:35:20 +0100

    move to chakra

--- src/ui/pages/NotFound.tsx
+++ src/ui/pages/NotFound.tsx
@@ -1,11 +1,11 @@
-import { Button, Image } from "react-bootstrap";
+import { Button, Image } from "@chakra-ui/react";
 import notFound from "../../assets/notFound.svg";
 
 export const NotFound = () => {
   return (
     <>
       <Image src={notFound} style={{ width: "32rem" }} />
-      <Button variant="primary" aria-label="home" href="/">
+      <Button variant="primary" aria-label="home">
         Go to the Homepage
       </Button>
     </>
